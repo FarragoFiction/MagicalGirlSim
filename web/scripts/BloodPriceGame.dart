3e5dc8c108409f0f9874c1ba10ec385f037dc8a8
jadedResearcher <gtg287y@gmail.com>
2019-04-27 14:55:40 -0400

    blood price

--- web/scripts/BloodPriceGame.dart
+++ web/scripts/BloodPriceGame.dart
@@ -20,6 +20,7 @@ class BloodPriceGame {
     MonsterGirl monsterGirl;
     Element secondMenu;
     Element thirdMenu;
+    Element thirdMenuInsides;
 
 
     BloodPriceGame({BloodPriceGirl this.currentGirl,MonsterGirl this.monsterGirl});
@@ -88,6 +89,12 @@ class BloodPriceGame {
     void displayBloodPriceSub2(Element container) {
         thirdMenu = new DivElement()..classes.add("bloodMenu2")..classes.add("menuHolder");
         container.append(thirdMenu);
+
+        final DivElement header = new DivElement()..text = "Blood Price"..classes.add("menuHeader");
+        thirdMenu.append(header);
+
+        thirdMenuInsides = new DivElement()..classes.add("menuInsides");
+        thirdMenu.append(thirdMenuInsides);
     }
 
     void displayMenu(Element container) {
@@ -148,7 +155,7 @@ class BloodPriceGame {
         menu.onClick.listen((Event e) {
             clickSound();
             thirdMenu.style.display = 'block';
-            thirdMenu.text = "TODO: Health Pact";
+            thirdMenuInsides.text = ("Trade future health for current health. Beware offering up more health than future you can spare.");
             unmarkChildren(container);
             menu.style.backgroundColor = "#fdbee5";
             menu.style.color = "#fff0f9";
@@ -161,7 +168,7 @@ class BloodPriceGame {
         menu.onClick.listen((Event e) {
             clickSound();
             thirdMenu.style.display = 'block';
-            thirdMenu.text = "TODO: Weapon Pact";
+            thirdMenuInsides.text =("Trade future health for current weapon strength. Beware offering up more health than future you can spare.");
             unmarkChildren(container);
             menu.style.backgroundColor = "#fdbee5";
             menu.style.color = "#fff0f9";
@@ -174,7 +181,7 @@ class BloodPriceGame {
         menu.onClick.listen((Event e) {
             clickSound();
             thirdMenu.style.display = 'block';
-            thirdMenu.text = "TODO: Magic Pact";
+            thirdMenuInsides.text =("Trade future health for current magic strength. Beware offering up more health than future you can spare.");
             unmarkChildren(container);
             menu.style.backgroundColor = "#fdbee5";
             menu.style.color = "#fff0f9";
@@ -187,7 +194,7 @@ class BloodPriceGame {
         menu.onClick.listen((Event e) {
             clickSound();
             thirdMenu.style.display = 'block';
-            thirdMenu.text = "TODO: Companion Pact";
+            thirdMenuInsides.text =("Trade future health for current companion strength. Beware offering up more health than future you can spare.");
             unmarkChildren(container);
             menu.style.backgroundColor = "#fdbee5";
             menu.style.color = "#fff0f9";
@@ -200,7 +207,7 @@ class BloodPriceGame {
         menu.onClick.listen((Event e) {
             clickSound();
             thirdMenu.style.display = 'block';
-            thirdMenu.text = "TODO: Legacy Pact";
+            thirdMenuInsides.text =("Trade future health for the ability to better help those who come after you. Beware offering up more health than future you can spare.");
             unmarkChildren(container);
             menu.style.backgroundColor = "#fdbee5";
             menu.style.color = "#fff0f9";
