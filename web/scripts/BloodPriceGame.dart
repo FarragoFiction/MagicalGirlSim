1b9ffcbb5ee0a0b7b75ce2e3afab0db08c01e43e
jadedResearcher <gtg287y@gmail.com>
2019-04-27 09:52:44 -0400

    monsters exist

--- web/scripts/BloodPriceGame.dart
+++ web/scripts/BloodPriceGame.dart
@@ -16,27 +16,39 @@ class BloodPriceGame {
         * menu shit???
      */
     BloodPriceGirl currentGirl;
+    MonsterGirl monsterGirl;
 
-    BloodPriceGame({BloodPriceGirl this.currentGirl});
+
+    BloodPriceGame({BloodPriceGirl this.currentGirl,MonsterGirl this.monsterGirl});
 
     void display(Element parent) async {
         if(currentGirl == null) {
             currentGirl = await BloodPriceGirl.randomGirl();
         }
 
+        if(monsterGirl == null) {
+            monsterGirl = await MonsterGirl.randomGirl();
+        }
+
         DivElement container = new DivElement()..classes.add("gameBox");
         parent.append(container);
+        displayMonster(container);
         displayCurrentGirl(container);
     }
 
     void displayCurrentGirl(Element container) async {
-        //TODO draw magical girl turnways on the left and half size
         currentGirl.doll.orientation = Doll.TURNWAYS;
         CanvasElement cacheCanvas = await currentGirl.doll.getNewCanvas();
         int ratio = 2;
         CanvasElement dollCanvas = new CanvasElement(width: (cacheCanvas.width/ratio).floor(), height: (cacheCanvas.height/ratio).floor());
         dollCanvas.context2D.drawImageScaled(cacheCanvas, 0,0, dollCanvas.width, dollCanvas.height);
         dollCanvas.classes.add("bloodDoll");
+        container.append(dollCanvas);
+    }
+
+    void displayMonster(Element container) async {
+        CanvasElement dollCanvas = await monsterGirl.doll.getNewCanvas();
+        dollCanvas.classes.add("monsterDoll");
         container.append(dollCanvas);
     }
 
@@ -50,5 +62,17 @@ class BloodPriceGirl extends MagicalGirlCharacterObject{
       await doll.setNameFromEngine();
       return new BloodPriceGirl(doll.dollName, doll.toDataBytesX());
   }
+
+}
+
+//TODO make actually a monster dollset not a magical girl (its a subset tho???)
+class MonsterGirl extends MagicalGirlCharacterObject{
+    MonsterGirl(String name, String dollString) : super(name, dollString);
+
+    static Future<MonsterGirl> randomGirl() async {
+        MagicalDoll doll = new MagicalDoll();
+        await doll.setNameFromEngine();
+        return new MonsterGirl(doll.dollName, doll.toDataBytesX());
+    }
 
 }
