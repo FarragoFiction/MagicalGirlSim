98f9edb076eba876400ce183f1fd3ea83bfe7021
jadedResearcher <gtg287y@gmail.com>
2019-04-28 16:41:31 -0400

    monster can fight back???

--- web/scripts/BloodPriceGame/MonsterGirl.dart
+++ web/scripts/BloodPriceGame/MonsterGirl.dart
@@ -2,14 +2,17 @@
 //TODO make actually a monster dollset not a magical girl (its a subset tho???)
 import 'dart:html';
 
+import 'package:CommonLib/Random.dart';
 import 'package:DollLibCorrect/DollRenderer.dart';
 
 import '../MagicalGirlCharacterObject.dart';
+import 'BloodPriceGame.dart';
 import 'BloodPriceGirl.dart';
 
 class MonsterGirl extends BloodPriceGirl{
     @override
     int hp = 999;
+    //TODO method to spawn a monster from a magical girl, blood pacts and all
     MonsterGirl(String name, String dollString) : super(name, dollString);
 
     static Future<MonsterGirl> randomGirl(MagicalDoll origin) async {
@@ -22,7 +25,20 @@ class MonsterGirl extends BloodPriceGirl{
     //wait three seconds and then do physical or magical damage to your opponent
     Future<void> takeTurn() async {
         await Future.delayed(Duration(milliseconds: 2000));
-        window.alert("???");
+        Random rand = new Random();
+        rand.nextInt(); //init
+        int damage = 0;
+        String type = "";
+        //companion damage now counts as monstrous strength owo
+        if(rand.nextBool()) {
+            damage = calculateWeaponDamage() + calculateCompanionDamge();
+            type = BloodPriceGame.PHYSICALDAMAGE;
+        }else {
+            damage = calculateMagicDamage() + calculateCompanionDamge();
+            type = BloodPriceGame.MAGICDAMAGE;
+
+        }
+        BloodPriceGame.instance.damageGirl(damage, type);
     }
 
 }
