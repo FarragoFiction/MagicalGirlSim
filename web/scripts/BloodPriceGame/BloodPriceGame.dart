ad6a9c13dcb88a708261a22a3879d5df5cd11e6f
jadedResearcher <gtg287y@gmail.com>
2019-04-29 17:45:03 -0400

    initial button

--- web/scripts/BloodPriceGame/BloodPriceGame.dart
+++ web/scripts/BloodPriceGame/BloodPriceGame.dart
@@ -221,6 +221,8 @@ class BloodPriceGame {
             spreadCorruption(cutsceneBirb, 8);
         }
         scene.append(cutsceneBirb);
+        ButtonElement button = new ButtonElement()..text = "Replay?";
+        scene.append(button);
         final DivElement scrollyThingy = new DivElement()..classes.add("autoscroller");
         scene.append(scrollyThingy);
         currentGirl.canvas.remove();
